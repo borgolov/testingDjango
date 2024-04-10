89831205e463dfd84be0b0bfdc8ed7efe3c5ec59
borgolov <79781059+borgolov@users.noreply.github.com>
2024-04-10 20:32:33 +0800

    init

---
+++ testingDjango/app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Element(models.Model):
+    name = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
