defa5bf116d56ddf9cbe29af34d8687579d7b194
borgolov <79781059+borgolov@users.noreply.github.com>
2024-04-14 20:41:31 +0800

    14.04.2024

--- testingDjango/app/models.py
+++ testingDjango/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Element(models.Model):
     name = models.CharField(max_length=200)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     def __str__(self):
         return self.name
--- testingDjango/app/views.py
+++ testingDjango/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Element
 
 # Create your views here.
 def home(request):
@@ -8,8 +9,35 @@ def home(request):
     
 
 def element(request, element_id):
-    if element_id is not None:
-        print(element_id)
-        return JsonResponse({ "id" : element_id })
-    else:
-        return JsonResponse({ "id" : 'None' })
+    # Получаем все элементы
+    elements = Element.objects.all()
+
+    # Создаем дерево элементов
+    element_tree = []
+    for element in elements:
+        if element.parent is None:  # Если элемент является корневым
+            element_tree.extend(get_element_tree(element))
+
+    # Вызываем функцию для печати дерева элементов
+    print_element_tree(element_tree)
+
+    return JsonResponse({'resp' : None})
+        
+
+def get_element_tree(element, level=0):
+    tree = [[element]]  # Первый элемент - это сам элемент, остальные - дочерние
+    for child in element.children.all():
+        child_tree = get_element_tree(child, level + 1)
+        tree[0].extend(child_tree)  # Добавляем дочерние элементы в список первого элемента
+    return tree
+
+
+def print_element_tree(tree, level=0):
+    for item in tree:
+        if isinstance(item, list):
+            # Это список дочерних элементов, печатаем их рекурсивно
+            print_element_tree(item, level + 1)
+        else:
+            # Это родительский элемент, печатаем его с отступами в зависимости от уровня
+            print('  ' * level + item.name)
+    
